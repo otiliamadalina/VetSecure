cc350308c97ce289d3bdc555632423c901808b6f
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-09-11 00:28:46 +0300

    feat: add fixed dark navbar with VetSecure logo

---
+++ frontend/src/layout/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Link, useNavigate} from "react-router-dom";
+import "../styles/navbar.css";
+
+export default function Navbar() {
+
+    return (
+        <nav className="navbar">
+            <div className="main-content">
+            <div className="container-fluid d-flex justify-content-start align-items-center">
+                <Link className="navbar-brand" to="/">
+                    <img src="/assets/VetSecure-Logo-White.png" alt="VetSecure Logo" className="navbar-logo" />
+                </Link>
+            </div>
+            </div>
+        </nav>
+
+    );
+
+}
