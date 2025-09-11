04b83a16e696e4f1b2f7adbe70b5de41002f3900
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-09-11 18:59:17 +0300

    fix: correct logo vertical alignment in navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Navbar from "./layout/navbar";
+import Home from "./pages/home/home";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-            <Navbar />
-            {/* <Footer /> */}
-        </BrowserRouter>
+        <Router>
+
+            <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+
+                </Routes>
+            {/*<Footer />*/}
+        </Router>
     </div>
   );
 }
--- frontend/src/layout/navbar.js
+++ frontend/src/layout/navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import {Link, useNavigate} from "react-router-dom";
 import "../styles/navbar.css";
+import "../styles/main.css";
 
 export default function Navbar() {
 
     return (
         <nav className="navbar">
-            <div className="container-fluid d-flex justify-content-start align-items-center">
-                <Link className="navbar-brand" to="/">
-                    <img src="/assets/VetSecure-Logo-White.png" alt="VetSecure Logo" className="navbar-logo" />
+            <div className="navbar-logo-container">
+                <Link to="/">
+                    <img src="/assets/VetSecure-Logo-Beige-Small.png" alt="Logo" className="navbar-logo" />
                 </Link>
             </div>
 
